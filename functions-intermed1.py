c73fe02bbd115dcb54d3ba1b130535d0d6a92d81
Joseph Hooker <jcolehooker@gmail.com>
2021-09-19 20:27:18 -0500

    User input now adds to dictionary instead of array; many edge cases accounted for

--- functions-intermed1.py
+++ functions-intermed1.py
@@ -1,41 +1,54 @@
 import random
 #input function with min as first value, max as second
 def inputMinMax():
-    a = []
+    a = {}
     #input receives two values max from user
     prompt_txt = "Please provide min value: "
     try:
         user_input = int(input(prompt_txt))
-        a.append(user_input)
+        a["min"] = user_input
     except:
-        print("Invalid entry")
+        print("Invalid entry, using default 0 value for min")
+        a["min"] = None
     prompt_txt = "Please provide max value: "
     try:
         user_input = int(input(prompt_txt))
-        a.append(user_input)
+        a["max"] = user_input
     except:
         print("Invalid entry")
+        a["max"] = None
     return a
 
 def randInt(min=0, max=100):
-    #edge case: min > max
-    if min > max:
+    if min < 0 and max < 0:
+    #edge case: min > max - both numbers provided by user
+        if min > max:
+            minTemp = 0
+            minTemp = min
+            min = max
+            max = minTemp
+            print("Max and min reversed!")
+        num = (random.random()*(min*-1)+(max*-1))*-1
+            
+    elif min > max:
         minTemp = 0
         minTemp = min
         min = max
         max = minTemp
         print("Max and min reversed!")
-    num = random.random()*max+min
-    # print(f"randInt Min Check: {min}, randInt Max Check: {max}")
+    else:
+        num = random.random()*max+min
     return round(num)
 #print(randInt()) 		    # should print a random integer between 0 to 100
 #print(randInt(max=50)) 	    # should print a random integer between 0 to 50
 #print(randInt(min=50)) 	    # should print a random integer between 50 to 100
 #print(randInt(min=50, max=500))    # should print a random integer between 50 and 500
 minMax = inputMinMax()
-if len(minMax) == 2:
-    print(randInt(min=minMax[0],max=minMax[1]))
-elif len(minMax) == 1: #Accounts for edge case of only one valid entry being provided, which gets used as min if < default max of 100, max if > 100
-    print(randInt(min=minMax[0]))
+if (minMax["min"] != None) and (minMax["max"] != None):
+    print(randInt(min=minMax["min"],max=minMax["max"]))
+elif (minMax["min"] == None) and (minMax["max"] != None): 
+    print(randInt(max=minMax["max"]))
+elif (minMax["min"] != None) and (minMax["max"] == None): 
+    print(randInt(min=minMax["min"]))
 else:
     print(randInt())
